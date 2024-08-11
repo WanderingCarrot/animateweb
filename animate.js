dee5c580bc8d93fed491f8fea743afdfac8968f5
WanderingCarrot <wooijunanimator@gmail.com>
2024-08-11 18:07:38 +0800

    Edited animate.js, index.html, style.css Added new images.

--- animate.js
+++ animate.js
@@ -1,15 +1,74 @@
-gsap.from('.title', {y: 150, opacity: 0, duration: 3, delay: 1})
-gsap.from('.title1', {y: 15, opacity: 0, duration: 0.78, ease: 'power1.out'})
-gsap.from('.text1', {y: 130, opacity: 0, duration: 3.75, delay: 1.5})
-gsap.from('.flag',{y: -150, opacity: 0, duration:2})
-gsap.from('.navbar-links', {x: 250, opacity: 0, duration: 2})
-gsap.fromTo('.year', {y: 100, opacity: 0},{y: -50, opacity: 0.35, duration: 2})
-gsap.fromTo('.class_img', {x: -100, opacity: 0}, {x: 5, opacity: 1, duration: 2})
-gsap.fromTo('.text2', {y: -50, opacity: 0}, {y: 5, opacity: 1, duration: 1})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-let smoother = ScrollSmoother.create({
-    wrapper: ".wrapper",
-    content: ".content",
-    smooth: 2,
-    effects: true,
-  });
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.title', {
+    y: 150, opacity: 0, duration: 3, delay: 1
+})
+gsap.from('.title1', {
+    scrollTrigger:{
+        trigger: '.title1',
+        toggleActions: "restart none none none"
+    },
+    x: -100, opacity: 0, duration: 0.78, ease: 'power1.out'
+})
+gsap.from('.text1', {
+    y: 130, opacity: 0, duration: 3.75, delay: 1.5
+})
+gsap.from('.flag',{
+    y: -150, opacity: 0, duration:2
+})
+gsap.from('.navbar-links', {
+    x: 250, opacity: 0, duration: 2
+})
+gsap.fromTo('.year', 
+    {y: -100, opacity: 0},
+    {y: 90, opacity: 0.35, duration: 2})
+gsap.fromTo('.class_img', 
+    {scrollTrigger:{
+        trigger: '.title1',
+        toggleActions: "restart none none none"
+    },
+        x: 100, opacity: 0}, 
+    {scrollTrigger:{
+        trigger: '.title1',
+        toggleActions: "restart none none none"
+    },
+        x: 5, opacity: 1, duration: 2})
+gsap.fromTo('.text2', 
+    {scrollTrigger:{
+        trigger: '.text2',
+        toggleActions: "restart none none none"
+    },
+    y: -50, opacity: 0}, 
+    {scrollTrigger:{
+        trigger: '.text2',
+        toggleActions: "restart none none none"
+    },
+    y: 5, opacity: 1, duration: 1})
+
+gsap.fromTo('.cleft', {
+    scrollTrigger:{
+        trigger: '.card-container',
+        toggleActions: "restart none none none"
+    }, x: -50, opacity: 0},{scrollTrigger:{
+        trigger: '.card-container',
+        toggleActions: "restart none none none"
+    }, x: 0, opacity: 1, duration: 3
+})
+gsap.fromTo('.cright', {
+    scrollTrigger:{
+        trigger: '.card-container',
+        toggleActions: "restart none none none"
+    }, x: 50, opacity: 0},{scrollTrigger:{
+        trigger: '.card-container',
+        toggleActions: "restart none none none"
+    }, x: 0, opacity: 1, duration: 3
+})
+gsap.fromTo('.footer', {
+    scrollTrigger:{
+        trigger: '.footer',
+        toggleActions: "restart none none none"
+    }, y: 100, opacity: 0},{scrollTrigger:{
+        trigger: '.footer',
+        toggleActions: "restart none none none"
+    }, y: 0, opacity: 1, duration: 2
+})
